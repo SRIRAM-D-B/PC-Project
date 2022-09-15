cba74eded256e6b54e42be8f379d2887bb433d50
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-15 09:58:11 -0700

    connected database -Sriram

--- Server/Const.js
+++ Server/Const.js
@@ -1,1 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const PORT = 4000;
+
+export const mongoUrl = `mongodb+srv://${process.env.DB_UserName}:${process.env.DB_Password}@pc.0oluxuf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,15 @@
 import Express from "express";
 import { PORT } from "./Const.js";
+import mongoose from "mongoose";
+import { mongoUrl } from "./Const.js";
 
 const app = Express();
 
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("server connected successfully!!!"))
+  .catch((err) => console.log("server connection failed!!!", err.message));
+
 app.listen(PORT, () => {
-  console.log(`Server active at ${PORT}`);
+  console.log(`Server listening at ${PORT}`);
 });
